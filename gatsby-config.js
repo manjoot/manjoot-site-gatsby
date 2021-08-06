46aaf496bc96d0909267bab81e3dca5428f85fdd
Manjoot <manjoot@hotmail.com>
2021-08-06 15:08:34 +0100

    Changing initial title to manjoot

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-postcss",
       options: {
-        postCssPlugins: [require("tailwindcss/jit"), require("autoprefixer")],
+        postCssPlugins: [require("tailwindcss"), require("autoprefixer")],
       },
     },
   ],
